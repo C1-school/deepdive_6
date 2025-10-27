f1564e4ef91dc12f95e955d8511b7d88e5dc8eb9
C1-school <pso97895@gmail.com>
2025-10-27 09:42:38 +0900

    learn value, data type change -window

---
+++ 1027.js
@@ -0,0 +1,15 @@
+function sum (a, b) {
+    return a+b;
+}
+
+console.log(sum(3, 5));
+console.log(sum(7, 10));
+
+const a = Number('5');
+console.log(typeof a, a);
+
+const b = String(3);
+console.log(typeof b, b);
+
+const c = Boolean(a+b);
+console.log(typeof c, c);
